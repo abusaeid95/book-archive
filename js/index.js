bf64e3fb0905756bc0ba4e0b5f8419d50da0123b
Abu Saeid <abusaeid95@gmail.com>
2021-09-03 21:01:03 +0600

    simple error

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
-
 const inputEmpty = document.getElementById('input-empty');
 const spinnerId = document.getElementById('spinner-id');
+const inputText = document.getElementById('input-search');
+const inputButton = document.getElementById('search-btn');
+
+// search value and handling error empty string
 const searchText =() =>{
+    error.textContent = ''; 
+    spinnerId.classList.remove('d-none');
     const inputValue = inputText.value;
-    
+    displayResult.textContent = '';
+    totalFound.textContent = ''; 
     if(inputValue ===''){
-        inputEmpty.classList.remove('d-none');
-        error.classList.add('d-none');
-        inputText.value = '';
+        inputText.value = ''; 
+        inputEmpty.classList.remove('d-none');       
         spinnerId.classList.add('d-none');
-        displayResult.textContent = '';
-        totalFound.textContent = '';
-        
-        
+
     }
     else{
+        inputEmpty.classList.add('d-none'); 
         displayResultURL(`https://openlibrary.org/search.json?q=${inputValue}`);  
     }     
 }
 
+//  Fetch URL
 const searchUrl =async url=>{
     const res = await fetch(url);
     const data = await res.json();
@@ -26,24 +30,24 @@ const searchUrl =async url=>{
 }
 const totalFound = document.getElementById('total-found'); 
 const displayResult = document.getElementById('visible-card');
-const error = document.getElementById('error-msg')
+const error = document.getElementById('error-msg');
+
+// Display Result and error handling
 const displayResultURL = url =>{
     searchUrl(url).then((data)=>{
         if(data.numFound===0){
-            error.classList.remove('d-none')
-            inputEmpty.classList.add('d-none');
             spinnerId.classList.add('d-none');
-            inputText.value = '';
+            error.innerText = `"${inputText.value}" : No Result found`;
             displayResult.textContent = '';
             totalFound.textContent = '';
+            inputText.value = ''; 
         }
         else{
+        inputText.value = ''; 
         displayResult.textContent = '';
         totalFound.textContent = '';
         spinnerId.classList.add('d-none')
         inputText.value = '';
-        inputEmpty.classList.add('d-none');
-        error.classList.add('d-none')
         const h1 = document.createElement('h1');
         h1.innerHTML = `<h1 class="m-5 fs-1">Total Book Found : ${data.numFound}</h1>`;
         totalFound.appendChild(h1);
@@ -66,9 +70,9 @@ const displayResultURL = url =>{
         }; 
     });
 };
-const inputText = document.getElementById('input-search');
-const inputButton = document.getElementById('search-btn');
+
+
+// Call function
 inputButton.addEventListener('click',function(){
     searchText();
-    spinnerId.classList.remove('d-none');
 });
